9c968a904017dbeba6628c01b0b77be3e0417ae6
iEdwinT <edwin_torres@icloud.com>
2020-04-15 22:45:48 -0400

    Initial commit

---
+++ src/jeopardyService.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+class JeopardyService {
+
+    constructor(url = 'https://jservice.kenzie.academy/api/random-clue', client = axios.create()){
+        this.url = url;
+        this.client = client;
+    }
+
+    getQuestion(){
+        return this.client.get(this.url);
+    }
+
+}
+
+export default JeopardyService;
